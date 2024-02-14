f6b05a762dcd4512799a4bf1cc3c1ad79b03f807
Ethan Gabriel <ethanbenjamin.gabriel@gmail.com>
2024-02-14 12:03:41 -0500

    finalizing login tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,17 +5,19 @@ describe('login to Script Runner Site', () => {
     cy.visit('/');
   });
 
-  it.skip('Successful login', () => {
+  it('Successful login', () => {
     cy.url().should('eq', 'https://staging.script-runner.com/');
     cy.get('[alt="script runner logo"]').should('be.visible');
     cy.get('button').should('be.visible').contains('Login').click();
 
     cy.origin('https://script-runner.us.auth0.com/', () => {
       cy.get('.auth0-lock-name').invoke('text').should('eq', 'Script Runner Login');
+
       cy.fixture('user').then((user) => {
         cy.get('#1-email').should('be.visible').type(user.username);
         cy.get('#1-password').should('be.visible').type(user.password);
       });
+
       cy.get('a[href*="#"]').invoke('text').should('eq', 'Don\'t remember your password?');
       cy.get('#1-submit').should('be.visible').click();
     });
@@ -30,31 +32,51 @@ describe('login to Script Runner Site', () => {
 
     cy.origin('https://script-runner.us.auth0.com/', () => {
       cy.get('.auth0-lock-name').invoke('text').should('eq', 'Script Runner Login');
+
       cy.fixture('user').then((user) => {
-        cy.get('#1-email').should('be.visible').type("test");
+        cy.get('#1-email').should('be.visible').type("test@script-runner.com");
         cy.get('#1-password').should('be.visible').type(user.password);
       });
+
       cy.get('a[href*="#"]').invoke('text').should('eq', 'Don\'t remember your password?');
       cy.get('#1-submit').should('be.visible').click();
-      cy.get('#auth0-lock-error-msg-email').invoke('text').should('eq', 'Email is invalid');
+      cy.get('.auth0-global-message').invoke('text').should('eq', 'Wrong email or password.');
+
+      cy.fixture('user').then((user) => {
+        cy.get('#1-email').should('be.visible').clear().type(user.username);
+      });
+
+      cy.get('#1-submit').should('be.visible').click();
     });
+
+    cy.url().should('include', 'dashboard');
   });
 
-  it.skip('Wrong Password login', () => {
+  it('Wrong Password login', () => {
     cy.url().should('eq', 'https://staging.script-runner.com/');
     cy.get('[alt="script runner logo"]').should('be.visible');
     cy.get('button').should('be.visible').contains('Login').click();
 
     cy.origin('https://script-runner.us.auth0.com/', () => {
       cy.get('.auth0-lock-name').invoke('text').should('eq', 'Script Runner Login');
+
       cy.fixture('user').then((user) => {
         cy.get('#1-email').should('be.visible').type(user.username);
-        cy.get('#1-password').should('be.visible').type("test");
+        cy.get('#1-password').should('be.visible').type("testPassword");
       });
+
       cy.get('a[href*="#"]').invoke('text').should('eq', 'Don\'t remember your password?');
       cy.get('#1-submit').should('be.visible').click();
-      cy.get('.auth0-global-message').invoke('text').should('eq', 'Email or password is incorrect');
+      cy.get('.auth0-global-message').invoke('text').should('eq', 'Wrong email or password.');
+
+      cy.fixture('user').then((user) => {
+        cy.get('#1-password').should('be.visible').clear().type(user.password);
+      });
+
+      cy.get('#1-submit').should('be.visible').click();
     });
+
+    cy.url().should('include', 'dashboard');
   });
 
   it('Forgot Password login', () => {
@@ -69,9 +91,11 @@ describe('login to Script Runner Site', () => {
       cy.get('.auth0-lock-header-welcome').invoke('text').should('eq', 'Reset your password');
       cy.get('.auth0-lock-body-content').invoke('text').should('include', 'Please enter your email address. We will send you an email to reset your password.');
       cy.get('.auth0-lock-input-email').should('be.visible');
+
       cy.fixture('user').then((user) => {
         cy.get('#1-email').should('be.visible').type(user.username);
       });
+
       cy.get('#1-submit').should('be.visible');
     });
   });
