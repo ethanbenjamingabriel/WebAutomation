623a53b625433bc9514489f6844662cf7ee64a77
Ethan Gabriel <ethanbenjamin.gabriel@gmail.com>
2024-02-14 16:37:35 -0500

    initial dashboard tests

---
+++ cypress/e2e/dashboard.cy.js
@@ -0,0 +1,81 @@
+describe('Navigate Dashboard Features', () => {
+
+    beforeEach(() => {
+      cy.viewport('macbook-13');
+      cy.login('/dashboard');
+    });
+  
+    it('Component Visibility', () => {
+        cy.get('[alt="script runner logo"]').should('be.visible');
+        cy.get('.jss5').should('be.visible');
+        cy.fixture('user').then((user) => {
+            cy.get('.MuiTypography-body1').should('be.visible').invoke('text').should('include', user.username);
+        });
+        cy.get('.jss7').should('be.visible');
+
+        cy.get('.jss12.MuiTypography-h1').invoke('text').should('eq', 'Start New Delivery');
+        cy.get('.MuiTypography-body1').invoke('text').should('include', 'Select a delivery date then scan the barcode and customer information for each new order.');
+        cy.get('.MuiButton-label').should('be.visible');
+
+        cy.get('.MuiTableRow-head').invoke('text')
+            .should('include', 'Date')
+            .should('include', 'Delivery Number')
+            .should('include', '# of Orders')
+            .should('include', 'Driver')
+            .should('include', 'Status')
+            .should('include', 'Returns');
+    });
+
+    it('Buttons & Dropdown Menu Functionality', () => {
+        cy.get('.jss7').should('be.visible').click();
+        cy.get('.MuiMenu-list').its('length').should('eq', 1);
+        cy.get('.MuiMenu-list').invoke('text').should('eq', 'Cards on FileLog out');
+        cy.get('.jss4').should('be.visible');
+        cy.get('.jss3').should('be.visible');
+
+        cy.get('.jss26').first().should('be.visible').click({ force: true });
+        cy.get('.MuiPickersBasePicker-container').should('be.visible');
+        cy.get('.jss26').last().should('be.visible').click({ force: true });
+        cy.get('.MuiPickersBasePicker-container').should('be.visible');
+
+        /*
+        cy.get('.MuiOutlinedInput-root.jss25').should('be.visible').dblclick({ force: true });
+        cy.get('.MuiMenu-list').invoke('text')
+            .should('include', 'All')
+            .should('include', 'Future')
+            .should('include', 'Present')
+            .should('include', 'Past');
+        */
+
+        cy.get('.MuiOutlinedInput-input').last().should('be.visible').type('test', { force: true });
+        cy.get('.jss39').click({ force: true });
+        cy.get('.jss39').click({ force: true });
+    });
+
+    it('Opening Delivery', () => {
+        var date = '';
+        var deliveryNumber = '';
+        var numOrders = '';
+        var driver = '';
+        
+        cy.get('.jss43').first().children().eq(0).invoke('text').then((val1) => {
+            date = val1;
+            cy.get('.jss43').first().children().eq(1).invoke('text').then((val2) => {
+                deliveryNumber = val2;
+                cy.get('.jss43').first().children().eq(2).invoke('text').then((val3) => {
+                    numOrders = val3;
+                    cy.get('.jss43').first().children().eq(3).invoke('text').then((val4) => {
+                        driver = val4;
+                        cy.get('.jss43').first().click();
+                        cy.get('.MuiPaper-elevation1').invoke('text').should('include', `Delivery Date: ${date}`);
+                        cy.url().should('include', `/dashboard/delivery/${deliveryNumber}`);
+                        cy.get('.MuiTypography-root.MuiTypography-body2').invoke('text').should('include', `Orders: ${numOrders}`);
+                        cy.get('.MuiPaper-elevation1').invoke('text').should('include', `Driver: ${driver}`);
+                    });
+                });
+            });
+        });
+        cy.get('.sc-gKPRtg.hruhHh').first().click();
+    });
+
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,10 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (url) => {
+    cy.visit(url);
+    cy.origin('https://script-runner.us.auth0.com/', () => {
+      cy.fixture('user').then((user) => {
+        cy.get('#1-email').type(user.username);
+        cy.get('#1-password').type(user.password);
+      });
+      cy.get('#1-submit').click();
+    });
+});
