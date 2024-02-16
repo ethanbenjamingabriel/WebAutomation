0bbf0cbc97d9df629dbead6d4d9c3d0013bf3326
Ethan Benjamin Gabriel <ethanbenjamin.gabriel@gmail.com>
2024-02-16 12:12:33 -0500

    Dashboard Tests (#3)
    * initial dashboard tests
    
    * adding third spec
    
    * revising dropdown spec
    
    * increasing container count
    
    * reducing container count
    
    * final change

---
+++ cypress/e2e/userdropdown.cy.js
@@ -0,0 +1,44 @@
+describe('Test User Dropdown Items', () => {
+
+    beforeEach(() => {
+        cy.viewport('macbook-13');
+        cy.login('/dashboard');
+      });
+
+    it('Log out of Script Runner from Dashboard', () => {
+        cy.get('.jss7').should('be.visible').click();
+        cy.get('.jss3').should('be.visible').click();
+        cy.url().should('eq', 'https://staging.script-runner.com/');
+        cy.get('[alt="script runner logo"]').should('be.visible');
+        cy.get('button').should('be.visible').contains('Login');
+    });
+
+    it('Cards on File Page', () => {
+        cy.get('.jss7').should('be.visible').click();
+        cy.get('.jss4').should('be.visible').click();
+
+        cy.url().should('include', 'settings/payment');
+        cy.get('.MuiTypography-h1').invoke('text').should('eq', 'Cards on fileAdd new card');
+        cy.get('[alt="Amex Icon"]').should('be.visible');
+        cy.get('[alt="MasterCard Icon"]').should('be.visible');
+        cy.get('[alt="Visa Icon"]').should('be.visible');
+
+        cy.get('.MuiOutlinedInput-input').eq(0).type('Ethan Gabriel');
+        cy.get('.MuiOutlinedInput-input').eq(1).type('Ethan');
+        cy.get('.MuiOutlinedInput-input').eq(2).type('Gabriel');
+
+        /*
+        cy.get('#cardNumber').type(4537123412341234);
+        cy.get('#expirationDate').type(1234);
+        cy.get('#cvv').type(123);
+        cy.get('#postalCode').type(12345);
+        cy.get('.sq-card-message sq-visible sq-card-message-error').invoke('text').should('eq', 'Enter a valid card number.');
+        */
+
+        cy.get('.MuiIconButton-label').eq(2).should('be.visible').click();
+        cy.get('.MuiButton-label').first().should('be.visible').invoke('text').should('eq', 'Cancel');
+        cy.get('.MuiButton-label').last().should('be.visible').invoke('text').should('eq', 'Proceed');
+        cy.get('.sc-ewvfhA.bLNHwQ').should('be.visible').invoke('text').should('eq', '+ Add new card');
+    });
+
+});
